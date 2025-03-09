01eaa74c3ddf618195f38cb7f44ba256c72e5d41
Yann Malet <yann.malet@gmail.com>
2025-03-09 16:26:44 +0100

    Explicitly using AsyncOpenAi client for Ollama
    `respond_to_user` method is always use in async context I force the use
    of an Async client.

--- chat.py
+++ chat.py
@@ -4,6 +4,7 @@ import sys
 from datetime import datetime
 from typing import List
 
+from openai import OpenAI, AsyncOpenAI
 from django.db import models
 from django.http import StreamingHttpResponse
 from mirascope import Messages, llm
@@ -24,24 +25,38 @@ app = Django(
 )
 
 
-@llm.call(provider="openai", model="gpt-3.5-turbo", stream=True)
-async def respond_to_user(user_message: str) -> Messages.Type:
-    return Messages.User(user_message)
-
-
-@app.admin
+@app.admin(
+    list_display=("provider", "model", "is_active"),
+    list_filter=("provider", "is_active"),
+)
 class AIModel(models.Model):
+    class Providers(models.TextChoices):
+        ANTHROPIC = ("anthropic", "Anthropic")
+        OLLAMA = ("ollama", "Ollama")
+        OPENAI = ("openai", "OpenAI")
+
+    provider = models.CharField(
+        max_length=100, choices=Providers, default=Providers.OLLAMA
+    )
     model = models.CharField(max_length=100)
-    provider = models.CharField(max_length=100)
+    base_url = models.CharField(max_length=255, blank=True)
+    stream = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.provider}:{self.model}"
 
+    def save(self, *args, **kwargs):
+        if self.is_active and (cnt := AIModel.objects.filter(is_active=True).count()):
+            raise ValueError(f"You can only have 1 active AIModel ({cnt})")
+        super().save(*args, **kwargs)
+
     class Meta:
         verbose_name = "AI Model"
         verbose_name_plural = "AI Models"
+        ordering = ("provider", "model")
 
 
 @app.admin
@@ -56,6 +71,28 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.role}: {self.content}"
+
+
+async def respond_to_user_with_default_model():
+    default_model: AIModel = await AIModel.objects.filter(is_active=True).afirst()
+    if default_model.provider == AIModel.Providers.OLLAMA and default_model.base_url and default_model.stream:
+        custom_client = AsyncOpenAI(
+            base_url=default_model.base_url,
+            api_key="FAKE_KEY",  # Not used by ollama but required
+        )
+    else:
+        custom_client = None
+
+    @llm.call(
+        provider=default_model.provider,
+        model=default_model.model,
+        stream=default_model.stream,
+        client=custom_client,
+    )
+    async def respond_to_user(user_message: str) -> Messages.Type:
+        return Messages.User(user_message)
+
+    return respond_to_user
 
 
 class MessageIn(app.ninja.Schema):
@@ -98,6 +135,7 @@ async def create_message(request, message: app.ninja.Form[MessageIn]):
         )
 
         bot_response = ""
+        respond_to_user = await respond_to_user_with_default_model()
         stream = await respond_to_user(user_message.content)
         async for chunk, _ in stream:
             if not bot_response and chunk.content:
